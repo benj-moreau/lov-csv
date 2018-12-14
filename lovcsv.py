6f57f5959e2802211deaa7a928983aa08dfb5d11
benjimor <benji_moreau@hotmail.fr>
2018-12-14 17:05:39 +0100

    suffixes fields, remove brackets

--- lovcsv.py
+++ lovcsv.py
@@ -30,7 +30,7 @@ except RuntimeError:
 def generate_classes_dataset():
     with open('lov_csv/lov_classes.csv', 'w') as f:
         writer = csv.writer(f, delimiter=";")
-        writer.writerow(['uri', 'label', 'description', 'language', 'occurencies_in_datasets', 'reused_by_datasets', 'sub_classes', 'equivalent_classes', 'defined_by'])
+        writer.writerow(['uri', 'uri_suffix', 'label', 'description', 'language', 'occurencies_in_datasets', 'reused_by_datasets', 'sub_classes', 'equivalent_classes', 'equivalent_classes_suffix', 'defined_by'])
         (classe_triples, cardinality) = LOV.search_triples("", RDF_TYPE, OWL_CLASS)
         (classe_triples2, cardinality2) = LOV.search_triples("", RDF_TYPE, RDF_CLASS)
         hdt_iterators = [classe_triples, classe_triples2]
@@ -39,25 +39,32 @@ def generate_classes_dataset():
             for classe_triple in hdt_iterator:
                 uri_classe = classe_triple[0].encode('utf-8')
                 if 'http' in uri_classe and uri_classe not in already_added_uri:
+                    uri_suffix = get_uri_suffix(uri_classe)
                     already_added_uri.append(uri_classe)
                     sub_classes = []
                     equivalent_classes = []
+                    equivalent_classes_suffix = [uri_suffix]
                     (sub_triples, cardinality) = LOV.search_triples(uri_classe, SUB_CLASSES, "")
                     for sub_triple in sub_triples:
                         if 'http' in sub_triple[2]:
                             sub_classes.append(sub_triple[2].encode('utf-8'))
                     if sub_classes:
-                        sub_classes = str(sub_classes)
+                        sub_classes = ", ".join(sub_classes)
                     else:
                         sub_classes = ''
                     (equi_triples, cardinality) = LOV.search_triples(uri_classe, EQUIVALENT_CLASSES, "")
                     for equi_triple in equi_triples:
                         if 'http' in equi_triple[2]:
                             equivalent_classes.append(equi_triple[2].encode('utf-8'))
+                            equivalent_classes_suffix.append(get_uri_suffix(equi_triple[2].encode('utf-8')))
                     if equivalent_classes:
-                        equivalent_classes = str(equivalent_classes)
+                        equivalent_classes = ", ".join(equivalent_classes)
                     else:
                         equivalent_classes = ''
+                    if equivalent_classes_suffix:
+                        equivalent_classes_suffix = ", ".join(equivalent_classes_suffix)
+                    else:
+                        equivalent_classes_suffix = ''
                     (defined_triples, cardinality) = LOV.search_triples(uri_classe, DEFINED_BY, "")
                     defined_by = ''
                     for defined_triple in defined_triples:
@@ -88,22 +95,22 @@ def generate_classes_dataset():
                     if labels_and_descriptions:
                         for language, value in labels_and_descriptions.iteritems():
                             if 'label' not in value:
-                                label = ''
+                                label = uri_suffix
                             else:
                                 label = value['label']
                             if 'description' not in value:
-                                description = ''
+                                description = label
                             else:
                                 description = value['description']
-                            writer.writerow([uri_classe, label, description, language, occurencies, reused, sub_classes, equivalent_classes, defined_by])
+                            writer.writerow([uri_classe, uri_suffix, label, description, language, occurencies, reused, sub_classes, equivalent_classes, equivalent_classes_suffix, defined_by])
                     else:
-                        writer.writerow([uri_classe, '', '', 'undefined', occurencies, reused, sub_classes, equivalent_classes, defined_by])
+                        writer.writerow([uri_classe, uri_suffix, uri_suffix, uri_suffix, 'undefined', occurencies, reused, sub_classes, equivalent_classes, equivalent_classes_suffix, defined_by])
 
 
 def generate_properties_dataset():
     with open('lov_csv/lov_properties.csv', 'w') as f:
         writer = csv.writer(f, delimiter=";")
-        writer.writerow(['uri', 'label', 'description', 'language', 'occurencies_in_datasets', 'reused_by_datasets', 'domain', 'range', 'sub_properties', 'equivalent_properties', 'defined_by'])
+        writer.writerow(['uri', 'uri_suffix', 'label', 'description', 'language', 'occurencies_in_datasets', 'reused_by_datasets', 'domain', 'range', 'sub_properties', 'equivalent_properties', 'equivalent_properties_suffix', 'defined_by'])
         (property_triples, cardinality) = LOV.search_triples("", RDF_TYPE, OWL_PROPERTY)
         (property_triples2, cardinality2) = LOV.search_triples("", RDF_TYPE, RDF_PROPERTY)
         hdt_iterators = [property_triples, property_triples2]
@@ -112,25 +119,32 @@ def generate_properties_dataset():
             for property_triple in hdt_iterator:
                 uri_property = property_triple[0].encode('utf-8')
                 if 'http' in uri_property and uri_property not in already_added_uri:
+                    uri_suffix = get_uri_suffix(uri_property)
                     already_added_uri.append(uri_property)
                     sub_properties = []
                     equivalent_properties = []
+                    equivalent_properties_suffix = [uri_suffix]
                     (sub_triples, cardinality) = LOV.search_triples(uri_property, SUB_PROPERTY, "")
                     for sub_triple in sub_triples:
                         if 'http' in sub_triple[2]:
                             sub_properties.append(sub_triple[2].encode('utf-8'))
                     if sub_properties:
-                        sub_properties = str(sub_properties)
+                        sub_properties = ", ".join(sub_properties)
                     else:
                         sub_properties = ''
                     (equi_triples, cardinality) = LOV.search_triples(uri_property, EQUIVALENT_PROPERTY, "")
                     for equi_triple in equi_triples:
                         if 'http' in equi_triple[2]:
                             equivalent_properties.append(equi_triple[2].encode('utf-8'))
+                            equivalent_properties_suffix.append(get_uri_suffix(equi_triple[2].encode('utf-8')))
                     if equivalent_properties:
-                        equivalent_properties = str(equivalent_properties)
+                        equivalent_properties = ", ".join(equivalent_properties)
                     else:
                         equivalent_properties = ''
+                    if equivalent_properties_suffix:
+                        equivalent_properties_suffix = ", ".join(equivalent_properties_suffix)
+                    else:
+                        equivalent_properties_suffix = ''
                     (defined_triples, cardinality) = LOV.search_triples(uri_property, DEFINED_BY, "")
                     defined_by = ''
                     for defined_triple in defined_triples:
@@ -169,16 +183,16 @@ def generate_properties_dataset():
                     if labels_and_descriptions:
                         for language, value in labels_and_descriptions.iteritems():
                             if 'label' not in value:
-                                label = ''
+                                label = uri_suffix
                             else:
                                 label = value['label']
                             if 'description' not in value:
-                                description = ''
+                                description = label
                             else:
                                 description = value['description']
-                            writer.writerow([uri_property, label, description, language, occurencies, reused, domain, p_range, sub_properties, equivalent_properties, defined_by])
+                            writer.writerow([uri_property, uri_suffix, label, description, language, occurencies, reused, domain, p_range, sub_properties, equivalent_properties, equivalent_properties_suffix, defined_by])
                     else:
-                        writer.writerow([uri_property, '', '', 'undefined', occurencies, reused, domain, p_range, sub_properties, equivalent_properties, defined_by])
+                        writer.writerow([uri_property, uri_suffix, uri_suffix, uri_suffix, 'undefined', occurencies, reused, domain, p_range, sub_properties, equivalent_properties, equivalent_properties_suffix, defined_by])
 
 
 def split_string_lang(obj):
@@ -188,6 +202,13 @@ def split_string_lang(obj):
     if len(splitted_obj) > 1:
         language = splitted_obj[1].encode('utf-8')
     return string, language
+
+
+def get_uri_suffix(typ):
+    if '#' in typ:
+        return typ.rsplit('#', 1)[-1]
+    else:
+        return typ.rsplit('/', 1)[-1]
 
 
 def main():
